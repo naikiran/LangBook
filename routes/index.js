3d2f041884fb754a8d413f1b66f887a73b92d12e
kiran <kirangohil652@gmail.com>
2025-06-11 10:41:35 +0530

    demo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.json({
+    status: 'success',
+    message: 'Welcome to LangBook API',
+    version: '1.0.0'
+  });
 });
 
 module.exports = router;
